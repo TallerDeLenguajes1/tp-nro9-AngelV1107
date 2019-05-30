abbe6dc8196434a1f9dbc5cb296ec9ffea59edcf
AngelV1107 <angelleandro@live.com>
2019-05-30 19:45:19 -0300

    Agregada funciones de Helper

--- TP9/TP9/Program.cs
+++ TP9/TP9/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Helpers;
+using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,34 @@ namespace TP9
     {
         static void Main(string[] args)
         {
+            string rutaCarpetaDestino;
+
+            SoporteParaConfiguracion.CrearArchivoDeConfiguracion(@"C:\Taller1-TP9-CarpetaMorse");
+
+            rutaCarpetaDestino = SoporteParaConfiguracion.LeerConfiguracion();
+
+            //Console.WriteLine(rutaCarpetaDestino);
+
+            foreach (string rutaArchivo in Directory.GetFiles(Directory.GetCurrentDirectory()))
+            {
+                //Console.WriteLine(archivo);
+                if ((Path.GetExtension(rutaArchivo) == ".mp3") || (Path.GetExtension(rutaArchivo) == ".txt"))
+                {
+                    if (!File.Exists(rutaCarpetaDestino + @"\" + Path.GetFileName(rutaArchivo)))
+                    {
+                        string rutaArchivoDestino = rutaCarpetaDestino + @"\" + Path.GetFileName(rutaArchivo);
+
+                        Console.WriteLine("Copiando {0} a {1}", Path.GetFileName(rutaArchivo), rutaArchivoDestino);
+
+                        File.Copy(rutaArchivo, rutaArchivoDestino);
+                    }
+                }
+            }
+
+            
+            
+
+            Console.ReadKey();
         }
     }
 }
--- TP9/TP9/SoporteParaConfiguracion.cs
+++ TP9/TP9/SoporteParaConfiguracion.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,33 @@ using System.Threading.Tasks;
 
 namespace Helpers
 {
-    class SoporteParaConfiguracion
+    public static class SoporteParaConfiguracion
     {
+        static string archivoConfig = "config.dat";
+
+        public static void CrearArchivoDeConfiguracion(string rutaCarpeta)
+        {
+            if (!Directory.Exists(rutaCarpeta))
+            {
+                Directory.CreateDirectory(rutaCarpeta);
+            }
+
+            if (!File.Exists(archivoConfig))
+            {
+                File.Create(archivoConfig);
+            }
+
+            BinaryWriter binario = new BinaryWriter(File.Open(archivoConfig, FileMode.Open));
+
+            binario.Write(rutaCarpeta);
+            binario.Close();
+        }
+
+        public static string LeerConfiguracion()
+        {
+            BinaryReader binario = new BinaryReader(File.OpenRead(archivoConfig));
+
+            return binario.ReadString();
+        }
     }
 }
